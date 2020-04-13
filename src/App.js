e106e9d8efb7a188afffc16403398c0ccb2353a0
John <johnmsaylor@gmail.com>
2020-04-13 13:36:55 -0400

    created a diplay function and imports from new mock file

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Post from "./components/Post";
+import posts from "./mock/examplePosts";
 
 class App extends Component {
-  post = {
-    posterName: "Arnell Milhouse",
-    resourceAuthor: "Albert Einstein",
-    jobSkillLevel: "Advanced",
-    cohort: "0",
-    title: "The Universal Equation",
-    categories: ["FORTRAN", "redux"],
-    summary: "This was a great article about blending fortran and redux.",
-    link: "www.google.com/search?q=Albert%20Einstein",
-    resourceType: "Article",
-    datePublished: "1943-10-03.00:00:00z",
-    videoLength: null,
-    timeToComplete: 3,
-    rating: 5,
-    comments: [{ commenter: "Cliff", text: "Nice post" }],
+  renderPosts = () => {
+    const display = posts.map((post) => {
+      return <Post post={post} />;
+    });
+    return display;
   };
+
   render() {
     return (
       <div className='App'>
-        <Post post={this.post} />
+        <h1>BRAIN HIVE</h1>
+        {this.renderPosts()}
       </div>
     );
   }
