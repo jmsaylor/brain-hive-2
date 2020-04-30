43884463c0725f548bd6c8bc350c551f698ce516
John <johnmsaylor@gmail.com>
2020-04-30 15:10:38 -0400

    fixed withRouter

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,43 +1,59 @@
 import React, { Component } from "react";
 import Post from "./Post";
-import { useParams } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import Button from "./common/Button";
 
 class SinglePost extends Component {
   constructor(props) {
     super(props);
+    const { postId } = this.props.match.params;
+    this.state = {
+      postId: postId,
+      showComments: false,
+    };
   }
-  // posts = posts.filter((elem) => elem.id === id);
-  // console.log("id:", id, typeof id);
 
-  // posts.forEach((element) => {
-  //   console.log(element.id, id);
-  // });
+  clickHandler = () => {
+    this.setState({
+      showComments: !this.state.showComments,
+    });
+  };
 
-  // console.log(posts);
+  componentDidMount() {
+    console.log(this.state);
+  }
 
   renderPosts = () => {
-    const { postId } = useParams();
-
+    const { postId } = this.state;
     const regexDigits = /\d*/;
     const regexWords = /^A-Z/;
-
     let { posts } = this.props;
-    posts = this.id.match(regexDigits)
+    console.log(posts);
+    posts = postId.match(regexDigits)
       ? posts.filter((element) => element.id === postId)
       : posts;
 
-    posts = this.id.match(regexWords)
+    posts = postId.match(regexWords)
       ? posts.filter((element) => element.resourceType === postId)
       : posts;
 
     const display = posts.map((post) => {
-      return <Post post={post} key={post.key} />;
+      return (
+        <div>
+          <Post post={post} key={post.key} />;
+          <Button onClick={() => this.clickHandler()}>
+            {this.state.showComments ? "Click to Hide" : "Click to show"}
+          </Button>
+          {this.state.showComments ? "Showing" : "Not showing"}
+        </div>
+      );
     });
     return display;
   };
+
   render() {
     return <div className='postList'>{this.renderPosts()}</div>;
   }
 }
 
-export default SinglePost;
+export default withRouter(SinglePost);
---
+++ src/components/common/Button.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <button style={myStyles.button} {...props}>
+      {props.children}
+    </button>
+  );
+};
+
+const myStyles = {
+  button: {
+    alignSelf: "center",
+    // backgroundColor: "#F96295",
+    backgroundColor: "#ff62a1",
+    marginLeft: 5,
+    marginRight: 5,
+    marginBottom: 10,
+    marginTop: 5,
+    width: "50%",
+    borderRadius: 10,
+    fontSize: 16,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+};
+
+export default Button;
